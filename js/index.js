7981e8c08e34a8263a1fb4613718c7150a3f7724
insekkei <insekkei@yahoo.com>
2015-01-23 16:49:08 +0800

    remove pic

--- js/index.js
+++ js/index.js
@@ -12,4 +12,10 @@ $(function(){
 		e.preventDefault();
 		$(this).parent('li').fadeOut(500);
 	});
+
+	$('#navigation li a').click(function(e){
+		$(this).parent('li').addClass('current').siblings().removeClass('current');
+	});
+	
+
 });
